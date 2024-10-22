b64ae82e41eb06ff7ca6a7f66fccc3ef979ab507
AbulQasimbintariq <www.qasimbhalli6547@gmail.com>
2024-10-22 21:24:13 +0500

    add function

--- index.js
+++ index.js
@@ -4,3 +4,6 @@ function firstfun() {
     let x = document.getElementById("yes");
     x.innerHTML = "Absolutely";
 }
+function printpage() {
+    print();
+}
--- index.ts
+++ index.ts
@@ -2,3 +2,6 @@ function firstfun(){
     let x = document.getElementById("yes");
     x.innerHTML = "Absolutely";
 }
+function printpage(){
+    print()
+}
